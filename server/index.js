e17c92a2cc681de1fa08f3508bd7b1aad9a54fd9
Nate Pucci <nathaniel.pucci@gmail.com>
2022-07-27 15:10:42 -0400

    tryin to connect files2

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express()
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 
