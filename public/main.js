3e6ed5adb8c0e148ac0713a83eac6acd24ae5772
Nate Pucci <nathaniel.pucci@gmail.com>
2022-07-28 15:08:41 -0400

    rollbar practice

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ const pizzabtn = document.getElementById('pizza-btn');
 const saladbtn = document.getElementById('salad-btn')
 const subsbtn = document.getElementById('subs-btn')
 const drinksbtn = document.getElementById('drinks-btn')
+const rollbarbtn = document.getElementById('rollbar')
 
 
 
@@ -9,3 +10,10 @@ pizzabtn.addEventListener('click', () => alert('Pizza!'))
 saladbtn.addEventListener('click', () => alert('Salad!'))
 subsbtn.addEventListener('click', () => alert('Subs!'))
 drinksbtn.addEventListener('click', () => alert('Drinks!'))
+rollbarbtn.addEventListener('click', goHome)
+
+const goHome = () => {
+  axios.get('http://localhost:4005/home').then(res => {
+  console.log('Nice work')
+})
+}
--- server/index.js
+++ server/index.js
@@ -4,13 +4,30 @@ const path = require('path');
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: 'cc64d550131b463386a1f4fbeb5a6fb1',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+app.get('/home', (req, res) => {
+    rollbar.log('Success!')
+    res.status(200).send('Nice work!')
+})
 
 app.use(express.static(path.join(__dirname, '../public')))
 
 const port = process.env.PORT || 4005
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
